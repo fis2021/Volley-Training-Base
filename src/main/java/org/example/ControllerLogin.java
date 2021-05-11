685ffd2a0477388a9fc10f457f5cc4f067da0bb6
acristinaflorea <cristina.florea@student.upt.ro>
2021-05-11 21:28:54 +0300

    Logare

--- src/main/java/org/example/ControllerLogin.java
+++ src/main/java/org/example/ControllerLogin.java
@@ -1,17 +1,41 @@
 package org.example;
-
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import java.io.IOException;
-
+import java.util.ArrayList;
 public class ControllerLogin {
     @FXML
     private Button register;
-
+    @FXML
+    private static UserModel loggedInUser = null;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    public void handleLogin() {
+        loggedInUser = null;
+        ArrayList<UserModel> userModels = Database.getAllUsers();
+        if(userModels == null)
+            new Alert(Alert.AlertType.ERROR, "No users have been registered yet").showAndWait();
+        else {
+            for(var i : userModels)
+                if(i.getUsername().equals(username.getText()) &&
+                        i.getPassword().equals(Database.encodePasswordMD5(password.getText()))) {
+                    loggedInUser = i;
+                    break;
+                }
+            if(loggedInUser == null)
+                new Alert(Alert.AlertType.ERROR, "Username or Password is incorrect").showAndWait();
+            else
+                System.out.println("Log in done");
+        }
+    }
     @FXML
     public void handleRegister() throws IOException {
         App.changeScene(register, "volleyRegister");
     }
-
 }
