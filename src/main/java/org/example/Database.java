c1342bd97916005ea7883b8f03a1bea58db936b9
Denisa-Floroiu <maria.floroiu@student.upt.ro>
2021-05-14 21:56:11 +0300

    Creare interfata

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import com.sun.source.tree.CatchTree;
 import javafx.scene.control.Alert;
 
 import java.security.MessageDigest;
@@ -53,6 +52,141 @@ public class Database {
             } catch(SQLException e) {
                 e.printStackTrace();
             }
+    }
+
+    // Uploads player data to db
+    public static void uploadPlayerData(PlayerData playerData) {
+        if(playerData != null)
+            try {
+                statement.execute(String.format("INSERT INTO playerData VALUES('%s', %d, '%s', %d)",
+                        playerData.getOwner_cnp(), playerData.getProgress(), playerData.getCoach(), playerData.getAbsents()));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        else
+            new Alert(Alert.AlertType.ERROR, "playerData variable cannot be null");
+    }
+
+    // Retrieves player data from db
+    public static PlayerData getPlayerData(String cnp) {
+        try {
+            statement.execute(String.format("SELECT * FROM playerData WHERE owner_cnp='%s'", cnp));
+            ResultSet resultSet = statement.getResultSet();
+            if(resultSet.next())
+                return new PlayerData(resultSet.getString("owner_cnp"), resultSet.getString("coach"),
+                        resultSet.getInt("progress"), resultSet.getInt("absents"));
+            else
+                return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Uploads parent data to db
+    public static void uploadParentData(ParentData parentData) {
+        if(parentData != null)
+            try {
+                statement.execute(String.format("INSERT INTO parentData VALUES('%s', %d, %d)",
+                        parentData.getOwner_cnp(), parentData.getAbsents(), parentData.getProgress()));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        else
+            new Alert(Alert.AlertType.ERROR, "playerData variable cannot be null");
+    }
+
+    public static void recruit(String cnp, String coachName) {
+        PlayerData playerData = Database.getPlayerData(cnp);
+        if(playerData == null)
+            new Alert(Alert.AlertType.ERROR, "Player doesn't exists").showAndWait();
+        else {
+            if(playerData.getCoach().equals("NONE")) {
+                try {
+                    statement.execute(String.format("UPDATE playerData SET coach='%s' WHERE owner_cnp='%s'",
+                            coachName, cnp));
+                } catch(SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+                new Alert(Alert.AlertType.ERROR, "Player already has a coach").showAndWait();
+        }
+    }
+
+    public static void progress(String cnp_player, int val) {
+        PlayerData playerData = Database.getPlayerData(cnp_player);
+
+        if(playerData == null)
+            new Alert(Alert.AlertType.ERROR, "Player doesn't exists").showAndWait();
+        else
+            try {
+                statement.execute(String.format("UPDATE playerData SET progress=%d WHERE owner_cnp='%s'",
+                        val, cnp_player));
+                ArrayList<UserModel> userModels = Database.getAllUsers();
+                if(userModels != null) {
+                    StringBuilder cnp_parent = new StringBuilder();
+                    for(var i : userModels)
+                        if(i.getCnp_child().equals(cnp_player)) {
+                            cnp_parent.append(i.getCnp());
+                            break;
+                        }
+
+                    if(!cnp_parent.toString().isEmpty())
+                        statement.execute(String.format("UPDATE parentData SET progress=%d WHERE owner_cnp='%s'",
+                                val, cnp_parent));
+                }
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public static void absent(String cnp) {
+        PlayerData playerData = Database.getPlayerData(cnp);
+        if(playerData == null)
+            new Alert(Alert.AlertType.ERROR, "Player doesn't exists").showAndWait();
+        else
+            try {
+                statement.execute(String.format("UPDATE playerData SET absents=%d WHERE owner_cnp='%s'",
+                        playerData.getAbsents() + 1, cnp));
+                ArrayList<UserModel> userModels = Database.getAllUsers();
+                if(userModels != null) {
+                    StringBuilder cnp_parent = new StringBuilder();
+                    for(var i : userModels)
+                        if(i.getCnp_child().equals(cnp)) {
+                            cnp_parent.append(i.getCnp());
+                            break;
+                        }
+
+                    if(!cnp_parent.toString().isEmpty()) {
+                        ParentData parentData = Database.getParentData(cnp_parent.toString());
+                        assert parentData != null;
+                        statement.execute(String.format("UPDATE parentData SET absents=%d WHERE owner_cnp='%s'",
+                                parentData.getAbsents() + 1, cnp_parent));
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+    }
+
+
+    // Retrieves parent data from db
+    public static ParentData getParentData(String cnp) {
+        try {
+            statement.execute(String.format("SELECT * FROM parentData WHERE owner_cnp='%s'", cnp));
+            ResultSet resultSet = statement.getResultSet();
+            if(resultSet.next())
+                return new ParentData(resultSet.getString("owner_cnp"), resultSet.getInt("absents"),
+                        resultSet.getInt("progress"));
+            else
+                return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // Returns all user from database
